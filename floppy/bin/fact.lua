cf162fcb448ec8394335c7ee23dca500f2b2cfa4
Creeper2009 <minecraftshadow46@gmail.com>
2014-08-06 14:13:20 -0700

    Create fact.lua

---
+++ floppy/bin/fact.lua
@@ -0,0 +1,21 @@
+-- feel free to alter
+facts = {
+	"One. Three. Two. Four. Nine. Ten.",
+	"Websites are designed in HTML.",
+	"OpenLuther was created in Lua.",
+	"This program was created in Lua.",
+	"This program is the best.",
+	"The first \"selfie\" ever taken is a picture of someone's lip.",
+	"BASIC was implemented in 1964.",
+	"4.",
+	"To contribute to OpenLuther development, make pull requests to the GitHub repository here: http://github.com/phillips1012/OpenLuther",
+	"-4707851673728401342",
+	"This is Lua code.\nvar = 1\nif(var == 1) then\nprint(\"yes\")\nend",
+	"OpenLuther was once a game written in JavaScript.",
+	"You can visit http://tech-talk.ml to talk with the OpenLuther developers, or talk with other forum members!",
+	"Minecraft seeds used to be based on your date and time.",
+	"\"Fact1.tableobject_o_dc\" not found.",
+	"The newline character is \"\\n\"."
+}
+
+print( facts[(math.random(1,#facts))] )
